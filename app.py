a7d556764250ee6378f866601a3c1a134548448b
jaewooli <jaeu1341@naver.com>
2025-01-07 01:21:13 +0900

    1/6

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,5 +9,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5050,debug=True)
     
